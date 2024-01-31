8cea8925740df81b3ddb5adbef82926726483715
anzyu-neu <122393965+anzyu-neu@users.noreply.github.com>
2024-01-31 13:43:10 -0700

    added logMSg() functino along with 2 objects to test proper errMsg detection

--- quiz/scripts/logger.js
+++ quiz/scripts/logger.js
@@ -1,1 +1,22 @@
 // Define a JavaScript function called logMsg() that can be used to log an error message for any object that contains the property errMsg. 
+const error = {
+    logMsg: function(obj) {
+        if (obj.errMsg != undefined) {
+            return obj.errMsg;
+        } else {
+            return "No errors\n";
+        }   
+    }
+}
+
+const obj = {
+    errMsg: "There is an error.",
+}
+
+const obj2 = {
+    firstName: "Andy",
+    lastName: "Yu"
+}
+
+console.log(error.logMsg(obj)); // Logs: "There is an error."
+console.log(error.logMsg(obj2)); // Logs: "No error."
