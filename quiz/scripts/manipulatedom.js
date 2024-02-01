47146fdb1f51aa38083ceac3633bfc4c0cc2d3ab
anzyu-neu <122393965+anzyu-neu@users.noreply.github.com>
2024-02-01 11:32:49 -0700

    modified to include edit text button and associated cell text

--- quiz/scripts/manipulatedom.js
+++ quiz/scripts/manipulatedom.js
@@ -6,7 +6,8 @@ function createTRNode(colNodes) {
   let trNode = document.createElement("tr");
   colNodes.forEach(function(colNode) {
     trNode.appendChild(colNode);
-  })
+  });
+
   return trNode;
 }
 
@@ -25,7 +26,33 @@ function addTable() {
   const tableNode = document.createElement("table");
   for(let i = 0; i < 3; i++) {
     let col1 = createTDNode(createTxtNode("Cell (" + i + ", 0)"));
-    tableNode.appendChild(createTRNode([col1]));
+    let col2 = createTDNode(createEditBtn(i, 0)) // add edit text button
+    tableNode.appendChild(createTRNode([col1, col2]));
   }
   document.getElementById("root").appendChild(tableNode);
+}
+
+function createEditBtn (x, y) {
+  let editBtn = document.createElement("button");
+  editBtn.innerText = "Edit text";
+  editBtn.onclick = function() {
+    editCell(x, y);
+  };
+  return editBtn;
+}
+
+function editCell(x, y) {
+  let table = document.querySelector("table");
+  let cell = table.rows[x].cells[y];
+  let inputField = document.createElement("input");
+    inputField.type = "text";
+    inputField.placeholder = "Enter Cell (x,y)...";
+    inputField.onfocus = function() {
+      if (inputField.value === inputField.placeholder) {
+        inputField.value = '';
+      }
+    };
+    cell.innerText = '';
+    cell.appendChild(inputField);
+    inputField.focus();
 }
