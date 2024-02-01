daa8ecd2cb6487252532924472b601f0660ec80b
anzyu-neu <122393965+anzyu-neu@users.noreply.github.com>
2024-02-01 12:24:58 -0700

    anonymous letters added

--- quiz/scripts/hideEmail-q.js
+++ quiz/scripts/hideEmail-q.js
@@ -7,5 +7,22 @@ window.onload = function hideEmail() {
     list.innerHTML = '';
     for (const email of emails) {
         // complete the loop
+        const atIndex = email.indexOf("@"); // acquire the index of the @ symbol
+        const domainNeu = "northeastern.edu";
+        let anonEmail = ''; // initialize the anonymous email with empty string
+        if (atIndex !== -1) { // if @ symbol exists
+            const username = email.slice(0, atIndex); // acquire the username
+            const domain = email.slice(atIndex + 1); // acquire the domain name  
+            const validUsername = /^[a-zA-Z0-9_]+$/.test(username); // check valid username input (chars, numbers, _);
+            const validDomain = domain.endsWith(domainNeu); // check that domain ends with northeastern.edu
+            if (validUsername && validDomain) {
+                anonEmail = '*'.repeat(username.length) + email.substring(atIndex); // replaces username with * and adds on domain 
+            }
+        }
+        // add email to the list of anonymous emails
+        const anonList = document.createElement('li'); // create new list 
+        anonList.textContent = anonEmail;
+
+        list.append(anonEmail);
     }
 }
